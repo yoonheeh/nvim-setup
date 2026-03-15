8e42a4d3158b5b5aa73c8227fa3732809962f50a
Yoon Hee Ha <yoonhee@bearrobotics.ai>
2026-03-15 16:01:28 -0400

    update editor settings: 2-space tabs, terminal styling, hide mode

--- lua/yoonhee/set.lua
+++ lua/yoonhee/set.lua
@@ -3,9 +3,9 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -16,3 +16,12 @@ vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
+vim.opt.showmode = false
+
+-- Use plain black background for terminal windows (no theme applied)
+vim.api.nvim_set_hl(0, "TermNormal", { bg = "#000000", fg = "#ffffff" })
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.wo.winhighlight = "Normal:TermNormal"
+  end,
+})
